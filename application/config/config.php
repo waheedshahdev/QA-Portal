6e22f05c21197f1c2534892cb099cb515a085335
Waheed Shah <waheedshah340@gmail.com>
2025-11-10 23:32:48 +0000

    Comment out base_url configuration

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = '';
-// $config['base_url'] = 'http://88.99.213.58/nw_solar/';
+// $config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
